88ea362cf268d561fa4ccfc145ab32125c67cde1
Alsouidan <alsouidan@gmail.com>
2019-02-28 16:21:47 +0200

    UUID

--- models/Entity_Emp.js
+++ models/Entity_Emp.js
@@ -1,11 +1,22 @@
+const uuidv4 = require('uuid/v4');
 class Entity_Emp {
     constructor(firstName,middleName,lastName,dateOfBirth,emp_type,form) {
+        this.id=uuidv4()
         this.dateOfBirth = dateOfBirth;
-        this.fullName=function() {
-            return this.firstName + " " +this.middleName+" "+this.lastName;}
+        // this.fullName=function() {
+            // return this.firstName + " " +this.middleName+" "+this.lastName;}
+        this.firstName=firstName
+        this.middleName=middleName
+        this.lastName=lastName
         this.emp_type=emp_type
         this.form=form
+        this.name=function(){return this.firstName+" "+this.middleName+" "+this.lastName}
+    //     this.age=function _calculateAge(birthday) { // birthday is a date
+    //         var ageDifMs = Date.now() - dateOfBirth.getTime();
+    //         var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    //         return Math.abs(ageDate.getUTCFullYear() - 1970);}
     };
+
 }
 
 module.exports = Entity_Emp
--- routes/api/entity_emp.js
+++ routes/api/entity_emp.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const Entity_Emp = require("../../models/Entity_Emp");
 
 const emp = [
-  new Entity_Emp("Ali", "Amr", "Souidan", new Date(98,2,14),"Lawyer","form A"),
+  new Entity_Emp("Ali", "Amr", "Souidan", new Date(98,02,14),"Lawyer","form A"),
+  new Entity_Emp("Ahmed", "Amr", "Souidan", new Date(98, 01, 13), "Reviewer", "form B"),
 ];
 
 router.get("/", (req, res) => res.json({ data: emp }));
