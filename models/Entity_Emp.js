09800b8cfda84b653cf4b61066af9c072e4e992e
Alsouidan <alsouidan@gmail.com>
2019-02-28 13:55:19 +0200

    Added forms to constructor

--- models/Entity_Emp.js
+++ models/Entity_Emp.js
@@ -1,10 +1,11 @@
 class Entity_Emp {
-    constructor(firstName,middleName,lastName,dateOfBirth,emp_type,) {
+    constructor(firstName,middleName,lastName,dateOfBirth,emp_type,form) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
         this.fullName=function() {
             return this.firstName + " " +this.middleName+" "+this.lastName;}
         this.emp_type=emp_type
+        this.form=form
     };
 }
 
