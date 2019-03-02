4e6015e54a8e44e783b05c3df893c404d39fbda6
Alsouidan <alsouidan@gmail.com>
2019-03-02 19:32:58 +0200

    CRUD COMMENTS DONE

--- models/Comment.js
+++ models/Comment.js
@@ -1,12 +1,12 @@
-const uuid = require('uuid');
+const uuid = require("uuid");
 class Comment {
-    constructor(author_type,author,text,read){
-        this.id = uuid.v4()
-        this.author_type=author_type
-        this.author=author
-        this.text=text
-        this.postedOn=new Date(new Date())
-        this.read=read
-    }
+  constructor(author_type, author, text, read_at) {
+    this.id = uuid.v4();
+    this.author_type = author_type;
+    this.author = author;
+    this.text = text;
+    this.postedOn = new Date();
+    this.read_at = new Date(read_at);
+  }
 }
-module.exports = Comment
+module.exports = Comment;
