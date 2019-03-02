5bd31ca8031d0e7bb62ca91c8a5cda095ac230fe
hbadr97 <hussein.badr@student.guc.edu.eg>
2019-03-02 14:37:33 +0200

    updated update form

--- routes/api/forms.js
+++ routes/api/forms.js
@@ -13,7 +13,7 @@ const forms = [
     new Form(0,"Laws drop down menu",
         "Legal form of company drop down",
         "SSC",
-        "لينا للانتاج",
+        "???? ???????",
         "Lina Productions",
         new Address("Apart2", "Sheikh Zayed", "Giza"),
         "012223533443",
@@ -27,7 +27,7 @@ const forms = [
     new Form(1, "Laws drop down menu",
         "Legal form of company drop down",
         "SPC",
-        "لينا للانتاج",
+        "???? ???????",
         "Lina Productions",
         new Address("Apart2", "Sheikh Zayed", "Giza"),
         "012223533443",
@@ -97,10 +97,9 @@ router.post('/post', async (req, res) => {
 
 
 //Updating a form
-router.post("/update", (req, res) => {
+router.put("/update/:id", (req, res) => {
 
-    console.log("0")
-    const id = req.body.id
+    const id = req.params.id
     const location = new Adress(req.body.location) //location contain town,city,address
     const arabicName = req.body.arabicName
     const englishName = req.body.englishName
@@ -113,78 +112,37 @@ router.post("/update", (req, res) => {
     const law = req.body.law
     const legalForm = req.body.legalForm
     const bitIL = req.body.bitIL
-    if (formType === "SPC") {
-        const schema = {
-            law: Joi.string().required(),//drop down menu
-            legalForm: Joi.string().required,//drop down menu
-            arabicName: Joi.string().required(), //make sure in next sprint that name of comapny is unique 
-            englishName: Joi.string(),
-            location: { address: Joi.string().required, city: Joi.string().required, town: Joi.string().required },
-            phone: Joi.string().length(11),
-            fax: Joi.string(),
-            capitalCurr: Joi.string().required().label('currency'),//drop down menu
-            capitalVal: Joi.number().positive().required().when(investor.nationality == ! "Egypt", {
-                is: true,
-                then: Joi.number().positive().max(100000).required()
-            }),
-            investor: Joi.string(), //validate all investor stuff at investor wait till he is made
-            bitIL: Joi.binary(),
-            formType: Joi.any().valid(['SPC', 'SSC']).required(),//drop down menu
-        }
-    }
-    else if (formType === "SSC") {
-        const cutoffDate = new Date(now - (1000 * 60 * 60 * 24 * 365 * 21));
-        const direcSchema = {
-            address: Joi.string().required(),
-            birthdate: Joi.date().max(cutoffDate).required(), // automatically put according to id, must be greater than 21 years
-            gender: Joi.any().valid(['male', 'female']).required(),//drop down menu
-            idNum: Joi.string().required().length(14),
-            name: Joi.string().required(),
-            nationality: Joi.string().required(),//drop down menu, manager must be egypt if investor foreign
-            position: Joi.string().required(),//drop down menu
-            typeID: Joi.any().valid(['passport', 'id']).required(),//drop down menu must be id if investor egypt
-            typeInves: Joi.any().valid(['individual', 'company']).required(),//drop down menu
-        }
-        const schema = {
-            law: Joi.string().required(),//drop down menu
-            legalForm: Joi.string().required,//drop down menu
-            arabicName: Joi.string().required(), //make sure in next sprint that name of comapny is unique 
-            englishName: Joi.string(),
-            location: { address: Joi.string().required, city: Joi.string().required, town: Joi.string().required },
-            phone: Joi.string().length(11),
-            fax: Joi.string(),
-            capitalCurr: Joi.string().required(),//drop down menu
-            capitalVal: Joi.number().min(50000).max(999999999999).required(),
-            investor: Joi.string(), //validate all investor stuff at investor wait till he is made
-            bitIL: Joi.binary(),
-            formType: Joi.any().valid(['SPC', 'SSC']).required(),//drop down menu maybe same as up
-            boardOfDirectors: Joi.array().min(1).items(Joi.object(direcSchema).required)
-        }
-    }
+    
     const result = Joi.validate(req.body, schema)
 
-    if (result.error) {
-        return res.status(400).send({ error: result.error.details[0].message })
-    }
-    const updatedForm = forms.find(function (user) {
-        console.log("1")
-        return user["id"] === id
-    })
+    try {
+        const isValidated = validator.updateValidation(req.body,formType)
+        if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
 
-    updatedForm["location"] = location
-    updatedForm["aracicName"] = arabicName
-    updatedForm["englishName"] = englishName
-    updatedForm["phone"] = phone
-    updatedForm["fax"] = fax
-    updatedForm["investor"] = investor
-    updatedForm["boardOfDirectors"] = boardOfDirectors
-    updatedForm["capitalCurr"] = capitalCurr
-    updatedForm["capitalVal"] = capitalVal
-    updatedForm["law"] = law
-    updatedForm["legalForm"] = legalForm
-    updatedForm["bitIL"] = bitIL
-    console.log("3");
-    return res.json({ data: updatedEmp });
+        const updatedForm = form.find(function(form){
+        
+        
+        updatedForm["location"] = location
+        updatedForm["aracicName"] = arabicName
+        updatedForm["englishName"] = englishName
+        updatedForm["phone"] = phone
+        updatedForm["fax"] = fax
+        updatedForm["investor"] = investor
+        updatedForm["boardOfDirectors"] = boardOfDirectors
+        updatedForm["capitalCurr"] = capitalCurr
+        updatedForm["capitalVal"] = capitalVal
+        updatedForm["law"] = law
+        updatedForm["legalForm"] = legalForm
+        updatedForm["bitIL"] = bitIL
+        return res.json({ data: updatedEmp })})
+        
+    }
+    
+    catch(error) {
+        // We will be handling the error later
+        console.log(error)
+    }  
+    
 
 });
 
