de01040ec914ef675d0d52241b82232d7582d82c
Petersamirx <peter.sedhom@student.guc.edu.eg>
2019-03-21 19:45:18 +0200

    external entity mongodb

--- models/external_entity.js
+++ models/external_entity.js
@@ -1,14 +1,26 @@
-const uuid = require('uuid')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-class external_entity {
-    constructor(name, address,telephone, fax, email) {
-        this.name = name;
-        this.address = address;
-        this.telephone = telephone;
-        this.fax = fax;
-        this.email=email;
-        this.id = uuid.v4();
-    };
-}
-
-module.exports = external_entity
+const external_entity_Schema = new Schema({
+  name: {
+      type: String,
+      required: true
+  },
+  address: {
+      type: String,
+      required: true
+  },
+  telephone: {
+      type: Number,
+      required: true
+  },
+  fax: {
+      type: Number, 
+      required: true
+  },
+  email: {
+      type: mongoose.SchemaTypes.Email,
+      required: true
+    }
+})
+module.exports = external_entity = mongoose.model('external_entities', external_entity_Schema) 
--- routes/api/external_entity.js
+++ routes/api/external_entity.js
@@ -1,73 +1,55 @@
-const express = require('express')
-const Joi = require('joi')
-const router = express.Router()
-const external_entity = require('../../models/external_entity')
+const express = require("express");
+const Joi = require("joi");
+const router = express.Router();
+const mongoose = require('mongoose')
+const external_entity = require("../../models/External_Entity");
+const validator = require("../../validations/external_entityValidations");
 
-const external_entity = [
-    new external_entity('external_entity1', 'address1', 24561987, 1513,"external_entity1@gmail.com"),
-    new external_entity('external_entity2', 'address2', 24561988, 1866,"external_entity2@gmail.com"),
-    new external_entity('external_entity3', 'address3', 24561989, 1867,"external_entity3@gmail.com")
-];
-
-router.get('/', (req, res) => res.json({ data: external_entity }))
-
-router.post('/', (req, res) => {
-	const name = req.body.name;
-    const address = req.body.address;
-    const telephone = req.body.telephone;
-    const fax = req.body.fax;
-    const email = req.body.email;
-
-	if (!name) return res.status(400).send({ err: 'Name field is required' })
-    if (typeof name !== 'string') return res.status(400).send({ err: 'Invalid value for name' })
-
-    if (!address) return res.status(400).send({ err: 'address field is required' })
-    if (typeof address !== 'string') return res.status(400).send({ err: 'Invalid value for address' })
-    
-	if (!telephone) return res.status(400).send({ err: 'telephone field is required' })
-    if (typeof telephone !== 'number') return res.status(400).send({ err: 'Invalid value for telephone' })
-
-    if (!fax) return res.status(400).send({ err: 'fax field is required' })
-    if (typeof fax !== 'number') return res.status(400).send({ err: 'Invalid value for fax' })
-    
-    if (!email) return res.status(400).send({ err: 'email field is required' })
-	if (typeof fax !== 'email') return res.status(400).send({ err: 'Invalid value for email' })
-
-	const newExternal_entity = {
-		name,
-        address,
-        telephone,
-        fax,
-        email,
-        id: uuid.v4()
-	}
-	return res.json({ data: newExternal_entity });
+router.get('/', async (req,res) => {
+  const entities = await external_entity.find()
+  res.json({data: entities})
 })
 
-router.put('/:id', (req,res) => {
-    try {
-        const id = req.params.id
-        const external_entity = await external_entity.findOne({id})
-        if(!external_entity) return res.status(404).send({error: 'external_entity does not exist'})
-        const isValidated = validator.updateValidation(req.body)
-        if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-        const updatedExternal_entity = await external_entity.updateOne(req.body)
-        res.json({msg: 'external_entity updated successfully'})
-       }
-       catch(error) {
-           console.log(error)
-       }  
-    })
-
-router.delete('/:id', async (req,res) => {
-    try {
-        const id = req.params.id
-        const deletedExternal_entity = await external_entity.findByIdAndRemove(id)
-        res.json({msg:'external_entity was deleted successfully', data: deletedExternal_entity})
+//create external entity
+router.post("/", async (req, res) => {
+  try{
+  const isValidated = validator.createValidation(req.body);
+  if (isValidated.error)
+    return res
+      .status(400)
+      .send({ error: isValidated.error.details[0].message })
+  const newExternal_entity = await external_entity.create(req.body)
+  return res.json({ data: newExternal_entity })
+  }
+  catch(error) {
+    console.log(error)
+}  
+});
+//update
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const external_entity = await external_entity.findOne({id})
+    if(!external_entity) return res.status(404).send({error: 'external_entity does not exist'})
+    const isValidated = validator.updateValidation(req.body)
+    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+    const updated_external_entity = await external_entity.updateOne(req.body)
+    return res.json({msg: 'Book updated successfully'})
+  } 
+  catch (error) {
+    console.log(error);
+  }
+});
+//delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleted_external_entity = await external_entity.findByIdAndRemove(id)
+    res.json({data: deleted_external_entity})
     }
-    catch(error) {
-        console.log(error)
-    }  
-    })
+  catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
