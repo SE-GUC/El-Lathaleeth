aaf159a421a707144d5ec69fdcdefd42a880cb0e
ElLaithy7 <alsouidan@gmail.com>
2019-03-23 15:13:06 +0200

    COmments read and create

--- routes/api/Comments.js
+++ routes/api/Comments.js
@@ -5,68 +5,43 @@ const Joi = require("joi");
 const Comment = require("../../models/Comment");
 const validator = require("../../validations/commentValidation");
 
-const comments = [
-  new Comment("Reviewer", "ay 7aga", "comment text", "2019-1-1", "read"),
-  new Comment("Lawyer", "ay 7aga2", "comment text2", "2019-3-15", "not read")
-];
+// const comments = [
+//   new Comment("Reviewer", "ay 7aga", "comment text", "2019-1-1", "read"),
+//   new Comment("Lawyer", "ay 7aga2", "comment text2", "2019-3-15", "not read")
+// ];
 
 router.post("/", async (req, res) => {
-  const author_type = req.body.author_type;
-  const author = req.body.author;
-  const text = req.body.text;
-  const read_at = req.body.read_at;
-
-  const isValidated = validator.createValidation(req.body);
-  if (isValidated.error)
-    return res
-      .status(400)
-      .send({ error: isValidated.error.details[0].message });
-
-  const newComment = new Comment(author_type, author, text, read_at);
-  comments.push(newComment);
-  return res.json({ data: newComment });
-});
-
-router.get("/", (req, res) => res.json({ data: comments }));
-router.get("/byID/:id", (req, res) => {
-  const id = req.params.id;
-  const com = comments.find(com => com.id === id);
-  res.json({ data: com })
-
-});
-
-
-
-router.delete("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  const com = comments.find(com => com.id === id);
-  const index = comments.indexOf(com);
-  if (index >= 0) {
-    comments.splice(index, 1);
+  try {
+    const isValidated = validator.createValidation(req.body);
+    if (isValidated.error)
+      return res
+        .status(400)
+        .send({ error: isValidated.error.details[0].message });
+    const com = await Comment.create(req.body);
+    res.json({ msg: "Comment was created successfully", data: com });
+  } catch (error) {
+    // We will be handling the error later
+    console.log(error);
   }
-  res.send(comments);
 });
 
-router.put("/update/:id", async (req, res) => {
-  const id = req.params.id;
-  const author_type = req.body.author_type;
-  const author = req.body.author;
-  const text = req.body.text;
-  const read_at = req.body.read_at;
-
-  const isValidated = validator.createValidation(req.body);
-  if (isValidated.error)
-    return res
-      .status(400)
-      .send({ error: isValidated.error.details[0].message });
-  const com = comments.find(com => com.id === id);
-  com["author_type"] = author_type;
-  com["author"] = author;
-  com["text"] = text;
-  com["read_at"] = read_at;
-
-  res.send(comments);
-  return res.json({ data: comments });
+router.get("/", async (req, res) => {
+  const com = await Comment.find();
+  res.json({ data: com });
 });
+router.get("/byID/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const com = await Comment.findById(id)
+    if (!com) return res.status(404).send({ error: "Comment does not exist" })
+    res.json({ msg: "Comment found", data: com });
+  }
+  catch (error) {
+    // Error will be handled later
+  }
+});
+
+
+
 
 module.exports = router;
--- validations/commentValidation.js
+++ validations/commentValidation.js
@@ -20,7 +20,14 @@ module.exports = {
     },
 
     updateValidation: request => {
-
-        return Joi.validate(request, updateSchema)
+        const direcSchema = {
+            author_type: Joi.string().valid(["Lawyer", "Reviewer"]).required(),
+            author: Joi.string().required(),
+            text: Joi.string().required(),
+            read_at: Joi.optional(),
+            postedOn: Joi.optional(),
+            id: Joi.optional()
+        }
+        return Joi.validate(request, direcSchema)
     },
 }
