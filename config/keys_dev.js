e22533058b22973f9a75a24a34ca2c99a15d6e45
osheriff <osheriff@protonmail.com>
2019-03-29 16:39:36 +0200

    Adding the grades

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Alsouidan:lathaleeth@cluster0-v0lev.mongodb.net/test?retryWrites=true"
-};
--- models/Admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const formScheme = require("./Form").schema;
-
-const AdminSchema = new Schema({
-  registered_investors: { type: formScheme }
-});
-
-module.exports = Admin = mongoose.model("admin", AdminSchema);
