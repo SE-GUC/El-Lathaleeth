fabeb241b7cfb98b1205a56f8f4a1e0b03177274
Alsouidan <alsouidan@gmail.com>
2019-03-31 00:40:46 +0200

    Entity_Emp tests done

--- entity_emp.test.js
+++ entity_emp.test.js
@@ -349,7 +349,7 @@ test('Testing Investor choosing to have laweyr fill form, then admin assigns a l
 },10000
 );
 test('Testing Investor choosing to have laweyr fill form, then admin assigns a lawyer, then lawyer fills form', async () => {
-    expect.assertions(3)
+    expect.assertions(2)
     const inv = await investor_funcs.createNewInvestor({
         firstName: "somefirstname",
         middleName: "somemiddlename",
@@ -428,9 +428,47 @@ test('Testing Investor choosing to have laweyr fill form, then admin assigns a l
         inv.data.data._id,
         adm.data.data._id
     );
-    const filled_form = await emp_funcs.lawyerFillForm(inv.data.data._id, adm.data.data._id,{
-
-    });
+    const filled_form = await emp_funcs.lawyerFillForm(
+      law.data.data._id,
+      inv.data.data._id,
+      {
+        formType: "SPC",
+        address: "Bouja",
+        arabicName: "???? ???????",
+        englishName: "Lina Productions",
+        phone: "11111111111",
+        fax: "23344",
+        investor: {
+          firstName: "Nadeen",
+          middleName: "Amr",
+          lastName: "Riad",
+          gender: "female",
+          nationality: "Egyptian",
+          investorType: "individual",
+          typeOfID: "id",
+          IDNumber: "1234567890",
+          dateOfBirth: "1970-03-21",
+          address: "Some place",
+          phoneNumber: "01117208627",
+          faxNumber: "1234A1234",
+          creditCardNumber: "4024007158885060",
+          email: "hello@gmail.com",
+          capital: "1000000",
+          capitalCurrency: "Euro",
+          investorFormID: inv.data.data._id
+        },
+        capitalCurr: "Euro",
+        capitalVal: 500000,
+        law: "Laws drop down menu",
+        legalForm: "Legal form of company drop down",
+        createdOn: "2019-03-02T19:55:25.722Z",
+        lastTouch: "_iddddd",
+        status: "active",
+        deadline: "2019-06-06",
+        bitIL: 0,
+        comments: []
+      }
+    );
     const new_law = await emp_funcs.getEntity_EmpbyID(law.data.data._id)
     expect(new_law.data.data.lawyer_details.to_be_filled_for).not.toContain(inv.data.data._id)
     expect(filled_form.data.data.investor.investorFormID).toEqual(
