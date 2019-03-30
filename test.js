074f80939b58378ce826c7e3f867341c22e3d1f5
YoussefSameh <youssef.gamaledin@student.guc.edu.eg>
2019-03-30 12:55:36 +0200

    enter

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ const emp_funcs = require('./funcs/entity_emp_funcs');
 const forms_func = require('./funcs/forms_funcs');
 const Form = require("./models/Form");
 const comment_funcs = require('./funcs/comment_funcs');
-const form_funcs = require('./funcs/form_funcs');
 
 test('', async () => {
     expect.assertions(1)
@@ -14,13 +13,30 @@ test('', async () => {
 test("Create Form", async ()=>{
   const formsLength = await Form.getForm();
   var form = {
-    "formType": "SSC",
+    "formType": "SPC",
     "address": "Bouja",
     "arabicName": "???? ???????",
     "englishName": "Lina Productions",
     "phone": "11111111111",
     "fax": "23344",
-    "investorId": "5c9cc93a90007226344b97a0",
+    "investor": {
+      "firstName": "Nadeen",
+      "middleName": "Amr",
+      "lastName": "Riad",
+      "gender": "Female",
+      "nationality": "Egyptian",
+      "investorType": "Individual",
+      "typeOfID": "Id",
+      "IDNumber": "1234567890",
+      "dateOfBirth": "1970-03-21",
+      "address": "Some place",
+      "phoneNumber": "01117208627",
+      "faxNumber": "1234A1234",
+      "creditCardNumber": "4024007158885060",
+      "email": "hello@gmail.com",
+      "capital": "1000000",
+      "capitalCurrency": "Euro"
+     },
     "capitalCurr": "Euro",
     "capitalVal": 500000,
     "law": "Laws drop down menu",
@@ -29,8 +45,7 @@ test("Create Form", async ()=>{
     "lastTouch": "_iddddd",
     "status": "active",
     "deadline": "2019-06-06",
-    "bitIL": 0,
-    "boardOfDirectors": ["5c9297b87061c221a85c7d15"]
+    "bitIL": 0
   }
   const response = await forms_func.createSSCForm(form);
   const formsLengthPlusOne = await Form.
