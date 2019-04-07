e2353eff73b5439b13c45b6449e6751867d85e8c
livaios <lina.eweis@student.guc.edu.eg>
2019-04-07 21:28:12 +0200

    Reviewer and Lawyer Worspace

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import LawyerPage from "./pages/LawyerPage";
 import lawyerPendingForms from "./pages/lawyerPendingForms";
+import lawyer_workspace from "./pages/lawyer_workspace";
+import reviewer_workspace from "./pages/reviewer_workspace";
+
 
 import "./App.css";
 const axios = require("axios");
@@ -18,11 +21,13 @@ class App extends Component {
           <ul className="header">
        <li><NavLink to="/LawyerPage">LawyerPage</NavLink></li>  
        <li><NavLink to="/lawyerPendingForms">My Pending Forms</NavLink></li>          
-        
+       <li><NavLink to="/lawyer_workspace">Lawyer Workspace</NavLink></li>
+       <li><NavLink to="/reviewer_workspace">Reviewer Workspace</NavLink></li>
           </ul>
           <Route path="/LawyerPage" component={LawyerPage} />
           <Route path="/lawyerPendingForms" component={lawyerPendingForms} />
-
+          <Route path="/lawyer_workspace" component={lawyer_workspace} />
+          <Route path="/reviewer_workspace" component={reviewer_workspace} />
         </div>
       </HashRouter>
     );
---
+++ client/src/pages/reviewer_workspace.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Cards from "../components/Cards";
+
+const axios = require("axios");
+
+class reviewer_workspace extends Component {
+  state = { pending_forms: [], reviewed_forms: []};
+
+  componentDidMount = async () => {
+    const reviewerinfo = await axios
+      .get("http://localhost:5000/api/entity_emp/workSpace/5ca9ea8fd0935b3388eaa962")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          pending_forms: res.data.pending_forms,
+          reviewed_forms: res.data.reviewed_forms
+        });
+      });
+  };
+
+  render() {
+    console.log(this.state);
+    const pending_forms = this.state.pending_forms;
+    const reviewed_forms = this.state.reviewed_forms;
+    
+    return (
+      <div className="reviewer_workspace">
+        <div className="Reviewer Pending Forms">
+          {pending_forms.length > 0 && <h4>Pending Forms:</h4>}
+          {pending_forms.length > 0 && <Cards forms={pending_forms}  tobereviewed={true}/>}
+        </div>
+        <div className="Reviewed Forms">
+          {reviewed_forms.length > 0 && <h4>Reviewed Forms:</h4>}
+          {reviewed_forms.length > 0 && <Cards forms={reviewed_forms}  tobereviewed={false}/>}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default reviewer_workspace;
