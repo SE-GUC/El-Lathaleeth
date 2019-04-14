ec0ddb700fe04a026305a2f2d041db40d18f2467
Alsouidan <alsouidan@gmail.com>
2019-04-14 15:44:25 +0200

    adding conditions to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -12,73 +12,82 @@ import "bootstrap/dist/css/bootstrap.css";
 import RegisterEmployee from "./pages/RegisterEmployee";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import { Provider } from "react-redux";
+import store from "./globalState/store";
+
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <body className="App">
-          <header>
-            <Navbar />
-          </header>
-          <main>
-            <Route exact path="/LawyerPage" component={LawyerPage} />
-            <Route
-              exact
-              path="/lawyerPendingForms"
-              component={lawyerPendingForms}
-            />
-            <Route exact path="/InvestorPage" component={InvestorPage} />
-            <Route exact path="/CasePage" component={CasePage} />
+      <Provider store={store}>
+        <HashRouter>
+          <body className="App">
+            <header>
+              <Navbar />
+            </header>
+            <main>
+              <Route exact path="/LawyerPage" component={LawyerPage} />
+              <Route
+                exact
+                path="/lawyerPendingForms"
+                component={lawyerPendingForms}
+              />
+              <Route exact path="/InvestorPage" component={InvestorPage} />
+              <Route exact path="/CasePage" component={CasePage} />
 
-            <Route
-              exact
-              path="/RegisterEmployee"
-              component={RegisterEmployee}
-            />
-            <Route exact path="/trackCasePage" component={trackCasePage} />
-            <Route
-              exact
-              path="/lawyer_workspace"
-              component={lawyer_workspace}
-            />
-            <Route exact path="/signin" component={SignIn} />
-            <Route exact path="/signup" component={SignUp} />
-          </main>
-          <footer class="page-footer grey darken-3 fixed bottom width-100%">
-            <div class="container">
-              <div class="row">
-                <div class="col l6 s12">
-                  <h5 class="white-text">About</h5>
-                  <p class="grey-text text-lighten-4">
-                    A website by the people for the people.
-                  </p>
-                </div>
-                <div class="col l4 offset-l2 s12">
-                  <h5 class="white-text">Links</h5>
-                  <ul>
-                    <li>
-                      <a class="grey-text text-lighten-3" href="#!">
-                        Meet Lathaleeth
-                      </a>
-                    </li>
-                    <li>
-                      <a class="grey-text text-lighten-3" href="#!">
-                        Contact us
-                      </a>
-                    </li>
-                  </ul>
+              <Route
+                exact
+                path="/RegisterEmployee"
+                component={RegisterEmployee}
+              />
+              <Route
+                exact
+                path="/trackCasePage"
+                component={trackCasePage}
+              />
+              <Route
+                exact
+                path="/lawyer_workspace"
+                component={lawyer_workspace}
+              />
+              <Route exact path="/signin" component={SignIn} />
+              <Route exact path="/signup" component={SignUp} />
+            </main>
+            <footer class="page-footer grey darken-3 fixed bottom width-100%">
+              <div class="container">
+                <div class="row">
+                  <div class="col l6 s12">
+                    <h5 class="white-text">About</h5>
+                    <p class="grey-text text-lighten-4">
+                      A website by the people for the people.
+                    </p>
+                  </div>
+                  <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                      <li>
+                        <a class="grey-text text-lighten-3" href="#!">
+                          Meet Lathaleeth
+                        </a>
+                      </li>
+                      <li>
+                        <a class="grey-text text-lighten-3" href="#!">
+                          Contact us
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="footer-copyright">
-              <div class="container align left">
-                © 2019 Sumerge feat.Lathaleeth
+              <div class="footer-copyright">
+                <div class="container align left">
+                  © 2019 Sumerge feat.Lathaleeth
+                </div>
               </div>
-            </div>
-          </footer>
-        </body>
-      </HashRouter>
+            </footer>
+          </body>
+        </HashRouter>
+      </Provider>
     );
   }
 }
--- client/src/components/layout/Sidenav.js
+++ client/src/components/layout/Sidenav.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
+import { connect } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import "materialize-css/dist/css/materialize.min.css";
 
@@ -13,35 +14,64 @@ class Sidenav extends Component {
   }
 
   render() {
-    return (
+    let displayed
+    let lawyerstuff = (
       <div>
-        <ul id="slide-out" className="sidenav">
-          <li />
-          <li>
-            <a className="subheader grey darken-3">Lawyer</a>
-          </li>
-          <li>
-            <a href="#/LawyerPage" class="sidenav-close">
-              LawyerPage
-            </a>
-          </li>
-          <li>
-            <a href="#/lawyer_workspace" class="sidenav-close">
-              Workspace
-            </a>
-          </li>
-          <li>
-            <a href="#/lawyerPendingForms" class="sidenav-close">
-              Pending Forms
-            </a>
-          </li>
-          <li>
-            <a href="#/CasePage">View All Cases</a>
-          </li>
-          <li>
-            <div className="divider" />
-          </li>
-          <li>
+        <li>
+          <a className="subheader grey darken-3">Lawyer</a>
+        </li>
+        <li>
+          <a href="#/LawyerPage" class="sidenav-close">
+            LawyerPage
+          </a>
+        </li>
+        <li>
+          <a href="#/lawyer_workspace" class="sidenav-close">
+            Workspace
+          </a>
+        </li>
+        <li>
+          <a href="#/lawyer_workspace" class="sidenav-close">
+            Fill Form for Investor
+          </a>
+        </li>
+        <li>
+          <a href="#/lawyerPendingForms" class="sidenav-close">
+            Pending Forms
+          </a>
+        </li>
+        <li>
+          <a href="#/CasePage">View All Cases</a>
+        </li>
+      </div>
+    );
+    let reviewerstuff = (
+      <div>
+        <li>
+          <a className="subheader grey darken-3">Lawyer</a>
+        </li>
+        <li>
+          <a href="#/LawyerPage" class="sidenav-close">
+            LawyerPage
+          </a>
+        </li>
+        <li>
+          <a href="#/lawyer_workspace" class="sidenav-close">
+            Workspace
+          </a>
+        </li>
+       
+        <li>
+          <a href="#/lawyerPendingForms" class="sidenav-close">
+            Pending Forms
+          </a>
+        </li>
+        <li>
+          <a href="#/CasePage">View All Cases</a>
+        </li>
+      </div>
+    );
+    let investorstuff=<div><li>
             <a className="subheader grey darken-3">Investor</a>
           </li>
           <li>
@@ -53,12 +83,9 @@ class Sidenav extends Component {
             <a href="#/trackCasePage" class="sidenav-close">
               Track my Case
             </a>
-          </li>
-          <li>
-            <div className="divider" />
-          </li>
-
-          <li>
+          </li></div>
+    let adminstuff=<div>
+      <li>
             <a className="subheader grey darken-3">Admin</a>
           </li>
           <li>
@@ -67,6 +94,37 @@ class Sidenav extends Component {
           <li>
             <a href="#/CasePage">View All Cases</a>
           </li>
+          </div>
+          try{
+          if(this.props.loggedUser.type==='Lawyer' ){
+            displayed=lawyerstuff
+          }else if (this.props.loggedUser.type==='Reviewer'){
+                        displayed = reviewerstuff;
+
+          }
+          else if (this.props.loggedUser.type==="Admin"){
+            displayed=adminstuff
+          }
+          else if (this.props.loggedUser.type==="investor"){
+            displayed=investorstuff
+          }
+          else{displayed=""}}
+          catch(e){displayed=""}
+    return (
+      <div>
+        <ul id="slide-out" className="sidenav">
+          {/* <li />
+          {lawyerstuff}
+          <li>
+            <div className="divider" />
+          </li>
+          {investorstuff}
+          <li>
+            <div className="divider" />
+          </li>
+
+          {adminstuff} */}
+          {displayed}
         </ul>
         <a
           href="#"
@@ -79,5 +137,13 @@ class Sidenav extends Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  isLoggedIn: state.auth.isLoggedIn,
+  loggedUser: state.auth.loggedUser
+});
 
-export default Sidenav;
+export default connect(
+  mapStateToProps
+)(Sidenav);
+// export default Sidenav
+
