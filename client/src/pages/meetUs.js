6328a92a7c112e9a780642eba5d46737f46e6385
Laithy <amrlaithy007@hotmail.com>
2019-04-21 19:05:18 +0200

    Meet us page

--- client/src/pages/meetUs.js
+++ client/src/pages/meetUs.js
@@ -5,38 +5,98 @@ const axios = require("axios");
 class meetUs extends Component {
     render(){
         return(
-            <div class="container">
-            <h1>Meet El Lathaleeth</h1>
-            <div class="gallery">
-                <figure class="gallery__item gallery__item--1">
-                    <img src="#" alt="Gallery image 1" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--2">
-                    <img src="#" alt="Gallery image 2" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--3">
-                    <img src="#" alt="Gallery image 3" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--4">
-                    <img src="#" alt="Gallery image 4" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--5">
-                    <img src="#" alt="Gallery image 5" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--6">
-                    <img src="#" alt="Gallery image 6" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--6">
-                    <img src="#" alt="Gallery image 7" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--6">
-                    <img src="#" alt="Gallery image 8" class="gallery__img"/>
-                </figure>
-                <figure class="gallery__item gallery__item--6">
-                    <img src="#" alt="Gallery image 9" class="gallery__img"/>
-                </figure>
-            </div>
+<div class="row">
+    <div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Ali Souidan</h4>
+    <p class="card-text">Some example text.</p>
         </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Lina Eweis</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Amr El-Laithy</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Nadeen Amr</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Hussein Badr</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Youssef El Sebaei</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Abdelrahman El Nakka</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Youssef Abougabal</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+<div class="column">
+        <div class="card card-custom mx-2 mb-3">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg" alt="Card image"/>
+  <div class="card-body">
+    <h4 class="card-title">Peter Samir</h4>
+    <p class="card-text">Some example text.</p>
+        </div>
+    </div>
+</div>
+
+</div>
         );
     }
 }
