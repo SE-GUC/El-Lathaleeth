944bcacf2da0f48efa96e3ff38c37675a6e18a09
YoussefSameh <youssef.gamaledin@student.guc.edu.eg>
2019-04-22 20:02:44 +0200

    carousel done

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "materialize-css/dist/css/materialize.min.css";
-import img1 from "../components/Assets/pyramidss.jpg"
-import img2 from "../components/Assets/Nile.jpeg"
-import img3 from "../components/Assets/nile2.jpg"
-import img4 from "../components/Assets/newCapp.jpg"
+import img1 from "../components/Assets/pyrrr.jpg"
+import img2 from "../components/Assets/Nileread.jpg"
+import img3 from "../components/Assets/nile2read.jpg"
+import img4 from "../components/Assets/NewCap.jpg"
+import img from "../components/Assets/bg.jpeg.jpg"
 
 class Carousel extends Component {
   componentDidMount() {
@@ -19,6 +20,10 @@ class Carousel extends Component {
   render() {
     return (
       <div className="carousel carousel-slider center" >
+      <a className="carousel-item" href="#">
+          {" "}
+          <img src= {img}  />
+        </a>
         <a className="carousel-item" href="#">
           {" "}
           <img src= {img1}  />
